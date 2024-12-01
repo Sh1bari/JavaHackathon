d3e2148e1fd0d3514047c35d9629d17d8c59738f
Vladimir Krasnov <vova_krasnov_2004@mail.ru>
2024-12-01 21:58:15 +0300

    compose

--- Main/src/main/java/com/example/main/services/FileService.java
+++ Main/src/main/java/com/example/main/services/FileService.java
@@ -2,16 +2,17 @@ package com.example.main.services;
 
 import com.example.main.models.entities.File;
 import com.example.main.models.entities.Person;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public interface FileService {
-
+    @Transactional
     public void deleteFile(File file);
 
     public MultipartFile convertBufferedImageToMultipartFile(BufferedImage bufferedImage, String fileName) throws IOException;
-
+    @Transactional
     public File uploadFile(MultipartFile file, Person person);
 }
--- Main/src/main/java/com/example/main/services/PersonService.java
+++ Main/src/main/java/com/example/main/services/PersonService.java
@@ -6,16 +6,17 @@ import com.example.main.models.request.CreatePersonDtoReq;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 public interface PersonService {
-
+    @Transactional
     public Person createPerson(CreatePersonDtoReq dto);
-
+    @Transactional
     public Page<Person> getPersons(Specification<Person> spec, Pageable pageable);
-
+    @Transactional
     public Person findById(UUID id);
-
+    @Transactional
     Person changePerson(ChangePersonDtoReq dto);
 }
